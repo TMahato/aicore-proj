3b9a689a7e66c445b7dabfff4d25cc7b08052c4d
Tanmay Mahato <tanmay.mahato@incture.com>
2025-04-23 10:25:09 +0530

    added dynamic algorithms

--- training/main.py
+++ training/main.py
@@ -1,41 +1,44 @@
 import os
 import pandas as pd
+import pickle
 
 # Variables
 DATA_PATH = '/app/data/irisdataset.csv'
-CLASS_LABEL= int(os.getenv('CLASS_LABEL'))
-KERNEL= os.getenv('KERNEL')
 MODEL_PATH = '/app/model/model.pkl'
 
-# Load Datasets
-import pandas as pd
+ALGORITHM = os.getenv('ALGORITHM', 'svm')
+CLASS_LABEL = float(os.getenv('CLASS_LABEL', 1.0))
+KERNEL = os.getenv('KERNEL', 'rbf')
+
+# Load Dataset
 df = pd.read_csv(DATA_PATH)
-# df = pd.read_csv('irisdataset.csv')
 X = df.drop('target', axis=1)
 y = df['target']
 
-# Partition into Train and test dataset
+# Train-test split
 from sklearn.model_selection import train_test_split
 train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3)
 
-# Init model
-from sklearn import svm
-model = svm.SVC(C=CLASS_LABEL, kernel=KERNEL)
-# model = svm.SVC(C=1, kernel="rbf")
+# Dynamic model initialization
+model = None
+if ALGORITHM == 'svm':
+    from sklearn import svm
+    model = svm.SVC(C=CLASS_LABEL, kernel=KERNEL)
+elif ALGORITHM == 'randomforest':
+    from sklearn.ensemble import RandomForestClassifier
+    model = RandomForestClassifier(n_estimators=int(CLASS_LABEL))
+elif ALGORITHM == 'logistic':
+    from sklearn.linear_model import LogisticRegression
+    model = LogisticRegression(C=CLASS_LABEL)
+else:
+    raise ValueError(f"Unsupported algorithm: {ALGORITHM}")
 
-# Train model
+# Train and evaluate
 model.fit(train_x, train_y)
-
-# Test model
 score = model.score(test_x, test_y)
-# Output will be available in logs of SAP AI Core.
-# Not the ideal way of storing /reporting metrics in SAP AI Core, but that is not the focus this tutorial
-print(f"Test Data Score: {score}")
+print(f"Test Accuracy: {score}")
 
 # Save model
-import pickle
-
-# Ensure the output directory exists
 os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
-
-pickle.dump(model, open(MODEL_PATH, 'wb'))
+with open(MODEL_PATH, 'wb') as f:
+    pickle.dump(model, f)
